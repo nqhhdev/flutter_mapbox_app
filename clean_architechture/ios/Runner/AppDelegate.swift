9057275c58af765ccc64ce69726413b76e91e859
Nguyen Huy Hoang <hoang.nguyen@vmodev.com>
2021-04-05 03:38:48 +0000

    Set-up firebase for base project

--- clean_architechture/ios/Runner/AppDelegate.swift
+++ clean_architechture/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,11 +8,13 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if #available(iOS 13.0, *) {
+       FirebaseApp.configure()
+     }
     let flavor = Bundle.main.infoDictionary?["Flavor"] as! String;
     print("Flavor : " + flavor);
 
     GeneratedPluginRegistrant.register(with: self)
-    
     let controller = self.window.rootViewController as! FlutterViewController
     let channel = FlutterMethodChannel.init(name: "flavor", binaryMessenger: controller.binaryMessenger)
     channel.setMethodCallHandler { (call, result) in
