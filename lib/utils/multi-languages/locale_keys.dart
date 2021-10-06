0e53ce6d415bfb98d760f85132fb751bc1d4d47e
Hob Nguyen <hob.nguyen@vmodev.com>
2021-10-06 13:03:10 +0700

    update readme and gitlab ci file

--- lib/utils/multi-languages/locale_keys.dart
+++ lib/utils/multi-languages/locale_keys.dart
@@ -6,5 +6,6 @@ part 'locale_keys.g.dart';
 @SheetLocalization(
   docId: '1SpiJWFRfJaIRnzpEc0mJ2WaaI9JYlz8jKBPduAPzdXE',
   version: 1,
+  //You must increment it each time you want to regenerate a new version of the labels.
 )
 class _LocaleKeys {}
