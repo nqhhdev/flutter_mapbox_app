8b897c36f4cd8d55cb02175b31b96d1663ee1451
Nguyen Huy Hoang <hoangnh@vmodev.com>
2022-03-10 09:34:57 +0000

    remove export on setup config

--- .setup_env_config.sh
+++ .setup_env_config.sh
@@ -7,18 +7,15 @@ if [ "$BUILD_FLAVOR_KEY" = "staging" ]; then
 elif [ "$BUILD_FLAVOR_KEY" = "production" ]; then
   flavor="Production"
 fi
-export BRANCH_NAME=$CI_BUILD_REF_NAME
-export COMMIT_TITLE=$CI_COMMIT_TITLE
-export USER_PERFORM=$GITLAB_USER_NAME
 
 echo Flavor "$flavor"
-echo Branch name "$BRANCH_NAME"
-echo Commit title "$COMMIT_TITLE"
-echo User name "$USER_PERFORM"
+echo Branch name "$CI_COMMIT_BRANCH"
+echo Commit title "$CI_COMMIT_TITLE"
+echo User name "$CI_COMMIT_AUTHOR"
 {
-echo BRANCH_NAME="$BRANCH_NAME"
-echo COMMIT_TITLE="$COMMIT_TITLE"
-echo USER_PERFORM="$USER_PERFORM"
+echo BRANCH_NAME="$CI_COMMIT_BRANCH"
+echo COMMIT_TITLE="$CI_COMMIT_TITLE"
+echo USER_PERFORM="$CI_COMMIT_AUTHOR"
 } >>fastlane/.env."$BUILD_FLAVOR_KEY"
 
 echo "========================Env $BUILD_FLAVOR_KEY File======================="
